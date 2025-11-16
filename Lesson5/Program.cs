5823324dfa889248649b174d05d0055203c33b78
Dan Zilberstein <dzilbers@gmail.com>
2025-11-16 20:29:39 +0200

    one more small example in lesson 5

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -1,6 +1,4 @@
-﻿using Lesson5;
-
-var oldArray = new[] { 2, 4, 6, 8, 10 };
+﻿var oldArray = new[] { 2, 4, 5, 8, 10 };
 var newArray =
     from number in oldArray
     where number % 2 == 0
@@ -15,14 +13,19 @@ foreach (var number in newArray)
     Console.Write(number + ",");
 Console.WriteLine();
 
-Console.WriteLine(newArray.Average());
+var realArray =
+    (from number in oldArray
+     where number % 2 == 0
+     select number * 2).ToList();
 
-List<Student> studentList =
-[
-    new Student {Id =  1, Name = "Yossi", Grades = [67, 90, 81]},
-    new Student {Id =  2, Name = "Dani", Grades = [100, 100, 100, 100]},
-    new Student {Id =  3, Name = "Eli", Grades = [61, 62, 63]},
-    new Student {Id =  4, Name = "Srulik", Grades = [73, 62, 81]}
-];
+//Console.WriteLine(newArray.Average());
 
-Console.WriteLine($"{studentList.Average(student => student!.Grades!.Average())}");
+//List<Student> studentList =
+//[
+//    new Student {Id =  1, Name = "Yossi", Grades = [67, 90, 81]},
+//    new Student {Id =  2, Name = "Dani", Grades = [100, 100, 100, 100]},
+//    new Student {Id =  3, Name = "Eli", Grades = [61, 62, 63]},
+//    new Student {Id =  4, Name = "Srulik", Grades = [73, 62, 81]}
+//];
+
+//Console.WriteLine($"{studentList.Average(student => student!.Grades!.Average())}");
