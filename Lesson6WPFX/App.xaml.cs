cebd4a05d0a3edb917f0bfc3915c322b1fec1104
Dan Zilberstein <dzilbers@gmail.com>
2025-12-19 15:07:27 +0200

    Update solution name

---
+++ Lesson6WPFX/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Lesson6WPF;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+    }
+}
